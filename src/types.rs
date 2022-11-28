c341cda8fbe7eb24a86b63c39c48f8ff69009604
Alexander <LoZander@outlook.com>
2022-11-28 05:26:25 +0100

    String of stuff

--- src/types.rs
+++ src/types.rs
@@ -21,20 +21,23 @@ impl std::fmt::Debug for Sym {
 
 
 pub fn string_of_char(c: &Char) -> String {
-    c.into_iter().map(|x| format!("{}",x)).collect()
+    c.into_iter()
+     .map(|x| format!("{}",x))
+     .collect::<String>()
 }
 
-#[allow(dead_code)]
 pub fn string_of_word(w: &Word) -> String {
-    let string: String = w.into_iter().map(|x| format!("{} ",string_of_char(x))).collect();
-    string.trim().to_string()
+    w.into_iter()
+     .filter(|x: &&Char| !x.is_empty())
+     .map(|x: &Char| format!("{} ",string_of_char(x)))
+     .collect::<String>()
+     .trim()
+     .to_string()
 }
 
-#[allow(dead_code)]
 pub fn string_of_sen(s: &Sen) -> String {
     let string = s.into_iter()
                           .map(|x| format!("{} / ",string_of_word(x)))
                           .collect::<String>();
-    string.as_str()[0..string.len() - 3]
-          .to_string()
+    string[0..string.len() - 3].to_string()
 }
