6c728772c6999d46c84256443575d84d34dde498
Alexander <LoZander@outlook.com>
2022-11-28 04:56:45 +0100

    Expanded morse lang

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod morse;
 mod parse;
 
 fn main() {
-    let plain = String::from("The quick brown fox jumps over the lazy dog, 1234567890. Certain character are not recognized and are simply thrown away, such as \"\'()[]{}.,:;/!?+-");
+    let plain = String::from("The quick brown fox jumps over the lazy dog, 1234567890. Certain character are not recognized and are simply thrown away, such as some of \"\'()[]{}.,:;/!?+-");
     println!("Plaintext: {}",plain);
     let encoded = encode(plain);
     println!("Encoded: {}", encoded);
