4430bdad5a4b5d775b78296470dd3b6b6eba02d2
Alexander <LoZander@outlook.com>
2022-11-29 22:47:04 +0100

    Introduced gui

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 use std::env;
-use eframe::egui;
 use morse::{MorseApp, MorseEncoder, MorseDecoder};
+use eframe::{egui};
 
 mod types;
 mod morse;
 mod parse;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        return
-    }
-    match args[1].as_str() {
-        "encode" => println!("{}",MorseApp::encode(args[2].clone())),
-        "decode" => println!("{}",MorseApp::decode(args[2].clone())),
-        s => println!("{} is not a valid command. Try \"encode <plaintext>\" or \"decode <ciphertext>\"", s)
-    }
+    let options = eframe::NativeOptions::default();
+    eframe::run_native("Morse", options, Box::new(|_cc| Box::new(MorseApp)));
 }
 
 impl eframe::App for MorseApp {
-    
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hellow world!");
+        });
+    }
 }
